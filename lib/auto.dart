0be1bd512cd808f08c04f757f220b36a581ddafa
MMMzq <vivask770@163.com>
2021-03-13 09:49:58 +0800

    docs: Update pubspec.yaml

--- lib/auto.dart
+++ lib/auto.dart
@@ -1,7 +1,5 @@
 library auto;
 
-import 'package:auto/src/log.dart';
-
 export 'src/ui_automator.dart' show UIAutomator, ProxyWidgetsFlutterBinding;
 export 'src/screenshot.dart' show takeScreenSnapshot;
 export 'src/config.dart';
